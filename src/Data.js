80d3704314a59082e4ddb7a37c780ea2c984c1df
dani01S <dany_mccalister@yahoo.com>
2020-04-07 20:16:55 +0300

    Products added to the data.js

--- src/Data.js
+++ src/Data.js
@@ -37,7 +37,66 @@ total:0
   inCart:false,
   count:0,
   total:0
+},
+{
+  id:4,
+  title:"Iphone 11 ",
+  img:'img/iphone-11.jpg',
+  price:3900,
+  company:'Iphone',
+  info:'Un nou telefon o noua experienta! Ia-ti noul iphone cu care captezi mai mult din ceea ce vezi cu noul sistem de camere. Poti face mai mult cu cel mai rapid chip de un smartphone, plus o durata de viata a bateriei care te rezista toata ziua. Iar amintirile tale vor arata mult mai bine datorita videoclipurilor de cea mai inalta calitate. Dragoste... La prima, a doua, a treia, a patra, a cincea, a sasea vedere. Telefonul are: techologie ecran liquid retina, memorie 64gb',
+  inCart:false,
+  count:0,
+  total:0
+},
+
+{
+  id:5,
+  title:"Samsung Galaxy S7",
+  img:'img/samsung-s7.jpg',
+  price:1200,
+  company:'Samsung',
+  info:'Samsung Galaxy J7 Elegant prin definitie .Cand stilul completeaza functionalitatea. Proiectat cu o mare atentie la detalii, noul Samsung Galaxy J7 are un finisaj metalic uimitor, fara vreo proeminenta a camerei in timp ce ecranul de sticla 2.5D protejeaza afisajul de 5.5 inch Full HD pentru o durabilitate sporita. Viata asa cum este',
+  inCart:false,
+  count:0,
+  total:0
+},
+
+{
+  id:6,
+  title:"Huawei 5T",
+  img:'img/Huawei-5t.jpg',
+  price:1500,
+  company:'Huawei',
+  info:'Telefon mobil Huawei 5T, Dual SIM, 128GB, 6GB RAM, 4G, Black Culoare care nu trece neobservata Culorile uluitoare, texturile nemaipomenite si designul reflector al telefonului HUAWEI nova 5T creeaza o senzatie de spatiu fara limite in care te cufunzi. Finisarea fina si textura unica accentueaza contrastul dintre lumini si umbre, oferind un efect 3D multi-layer fantastic, ca din alta lume, pe o suprafata plana. Deblocheaza intr-o secunda telefonul',
+  inCart:false,
+  count:0,
+  total:0
+},
+{
+  id:7,
+  title:"Xiaomi Redmi",
+  img:'img/xiaomi.jpg',
+  price:500,
+  company:'Xiaomi',
+  info:'telefon mobil Xiaomi Redmi 8A, Dual SIM, 32GB, 4G, Midnight Black Redmi 8A: baterie de mare capacitate de 5000 mAh. Timp de standby super-lung de pana la 31 de zile. Suporta incarcare rapida de 18 W',
+  inCart:false,
+  count:0,
+  total:0
+},
+{
+  id:8,
+  title:"Galaxy Fold",
+  img:'img/Galaxy-Fold.jpg',
+  price:8500,
+  company:'Samsung',
+  info:'Telefon mobil Samsung Galaxy Fold, 512GB, 12GB RAM, 4G, Cosmos Black Cea mai noua inovatie in materie de Smartphone-uri, astazi viitorul se desfasoara cu noul Samsung Galaxy Fold. Display-ul pliabil Infinity Flex .Un uimitor ecran dinamic AMOLED de 7,3 inci care sfideaza asteptarile. Fabricat dintr-un polimer revolutionar stratificat pentru o flexibilitate subtire, este cel mai mare ecran de smartphone Galaxy. Doua dimensiuni a display-ului pe un singur dispozitiv',
+  inCart:false,
+  count:0,
+  total:0
 }
+
+
 
 ];
 
--- src/components/Default.js
+++ src/components/Default.js
@@ -5,7 +5,7 @@ export default class Navbar extends Component{
   render(){
     return(
       <div>
-       <h3>PAGE NOT FOUND</h3>
+       <h3 className="text-center">EROR 404 : PAGE NOT FOUND</h3>
       </div>
     )
   }
